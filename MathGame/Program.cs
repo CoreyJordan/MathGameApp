38b90ab21a023215b798465542b1d53123b3e0c7
Corey Jordan <113132164+CoreyJordan@user.noreply.github.com>
2023-04-19 20:01:21 -0400

    Add WelcomeUser()

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,5 +1,9 @@
-﻿
+﻿WelcomeUser();
 
-
-
-
+static void WelcomeUser()
+{
+    Console.WriteLine("Welcome to Math Quizzer!");
+    Console.WriteLine();
+    Console.WriteLine("Created by Corey Jordan, 2023");
+    Console.WriteLine("Designed for The C# Academy \"Math Game\"");
+}
