f25618a71197ac34a99bda2cf9ee219031b93106
Corey Jordan <113132164+CoreyJordan@user.noreply.github.com>
2023-04-19 20:08:08 -0400

    feat UI: Add GetUserName()

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,4 +1,16 @@
 ﻿WelcomeUser();
+string playerName = GetUserName();
+
+string GetUserName()
+{
+    string? name;
+    do
+    {
+        Console.Write("Please enter your name: ");
+        name = Console.ReadLine()!;
+    } while (name == string.Empty);
+    return name;
+}
 
 static void WelcomeUser()
 {
@@ -6,4 +18,5 @@ static void WelcomeUser()
     Console.WriteLine();
     Console.WriteLine("Created by Corey Jordan, 2023");
     Console.WriteLine("Designed for The C# Academy \"Math Game\"");
+    Console.WriteLine();
 }
